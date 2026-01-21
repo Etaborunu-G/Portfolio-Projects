50b49c0246a92a3dd8dc32d1507035c307cc8fd0
Gaetano Etaborunu <161640341+Etaborunu-G@users.noreply.github.com>
2026-01-21 16:02:00 -0500

    Add files via upload

---
+++ Redvault Encryption/style.py
@@ -0,0 +1,23 @@
+# style.py
+DARK_RED_THEME = """
+QWidget {
+    background-color: #0f0f12;
+    color: #e7e7e7;
+    font-family: Segoe UI;
+}
+QPushButton {
+    background-color: #1b1b24;
+    border-radius: 10px;
+    padding: 8px;
+}
+QPushButton#primary {
+    background-color: #ff2d2d;
+    color: black;
+    font-weight: bold;
+}
+QLineEdit {
+    background-color: #15151c;
+    border-radius: 8px;
+    padding: 6px;
+}
+"""
